d64885fe822cb31da2fd9457353909bc08d8128c
clean_water <shimizyuta213@gmail.com>
2021-07-15 02:38:04 +0900

    エラー処理追加

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
 
-
 const PORT = process.env.PORT || 3000;
+
+
+
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.sendStatus(err.status || 500);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
