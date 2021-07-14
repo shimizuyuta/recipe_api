476054bed94079ce26bd10d68c2f50a838945a57
clean_water <shimizyuta213@gmail.com>
2021-07-15 03:48:20 +0900

    切り分け

--- api/index.js
+++ api/index.js
@@ -3,9 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const router = require('./router')
-
 const PORT = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  next();
+});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use('/',router)
 
 
