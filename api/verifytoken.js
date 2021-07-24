39a4ec59d621d630fcd77a92e5282258821f518c
clean_water <shimizyuta213@gmail.com>
2021-07-24 09:53:13 +0900

    verifytoken 修正

--- api/verifytoken.js
+++ api/verifytoken.js
@@ -1,28 +1,35 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+
 const verifyToken = (req,res,next) =>{
     const authHeader = req.headers["authorization"];
     if((authHeader !==undefined) && (authHeader.split(" ")[0] === "Bearer")){
         try{
            const token =authHeader.split(" ")[1];
-           console.log('token_______',authHeader.split(' ')[1])
 
            jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
-                if(req.session.token===authHeader.split(" ")[1]){
+                if((!err)&&(req.session.token===authHeader.split(" ")[1])){
                     next()
-                }else{
-                    return res.status(503).send({message:err.message})
+                }
+                else{
+                    return res.status(503).send({message________:err})
                 }
               
            })         
 
-        }catch(e){
-            return res.status(401).send({message:`invalid token ${e.message}`})
+        }
+        catch(e){
+            return res.status(401).send({message:`ログインしてください`})
         }
     }else{
         return res.status(401).send({message:`invalid token `})
     }
 }
+
+const generateToken = (userData)=>{    
+    return jwt.sign({id:userData.id},process.env.JWT_SECRET,{expiresIn:120})
+}
 module.exports={
-    verifyToken:verifyToken
+    verifyToken:verifyToken,
+    generateToken:generateToken
 }
